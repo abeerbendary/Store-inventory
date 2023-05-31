1376af0bbcebeff58789b0e530f956902164b37a
abeerbendary <abeerbendary16@gmail.com>
2023-05-31 10:46:26 +0300

    add login

---
+++ app/src/main/java/com/imdaad/store_inventory/Login.java
@@ -0,0 +1,14 @@
+package com.imdaad.store_inventory;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
